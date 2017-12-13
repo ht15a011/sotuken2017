db21d8d76ace7ba10ce69de0a2489b48ae26cf1a
uemoto <ht15a011@oecu.jp>
2017-12-13 20:55:29 +0900

    edit suriasi motion program

--- src/khr-3hv_heikou-suriasi-motion.py
+++ src/khr-3hv_heikou-suriasi-motion.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding: utf-8
 
 import sys
 import os
@@ -8,23 +9,36 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 #
 # Functions.
-#
+# 並行摺足の動作を1回行う関数
 
 def publish_khr_3hv_jointtrajectory(khrdeg = 0):
-    rad = math.radians(khrdeg)
+    oneDeg_rad = math.radians(1)		# 1度のラジアンを計算して，変数に格納
+    rad = math.radians(khrdeg)			# 関節を曲げる角度をラジアンに変更
     pub = rospy.Publisher('/KHR_3HV/torque_control/set_joint_trajectory', JointTrajectory, latch=True, queue_size=10)
     rospy.init_node('khr_3hv__choreonoid_ros', anonymous=True)
     msg = JointTrajectory()
-    msg.joint_names = [ khrname ]
+    
+    Lhips = 'body_2_to_Lhips'
+    Rhips = 'body_2_to_Rhips'
+	
+    msg.joint_names = [ Lhips ]
     msg.points = []
     p = JointTrajectoryPoint()
     p.time_from_start = rospy.rostime.Duration(0)
-    p.positions = [ rad ]
-
-    msg.points.append(p)
-    rospy.loginfo(msg)
-    pub.publish(msg)
-    rospy.Rate(1).sleep()
+    
+    bend = 0.0
+    while bend == khrdeg:
+		if khrdeg < 0:
+			p.positions = [ bend ]
+			bend -= oneDeg_rad
+		else:
+			p.positions = [ bend ]
+			bend += oneDeg_rad
+		msg.points.append(p)
+		rospy.loginfo(msg)
+		pub.publish(msg)
+		r = rospy.Rate(10)
+		r.sleep()
 
 def usage():
     name = os.path.basename(sys.argv[0])
@@ -46,7 +60,7 @@ if __name__ == '__main__':
 
     try:
         if len(sys.argv) == 2:
-            jointDeg = float(sys.argv[1])  # set joint deg
+            jointDeg = float(sys.argv[1])  # set joint degree
 
             if (-30.0 <= jointDeg and jointDeg <= 30.0):
                 publish_khr_3hv_jointtrajectory(khrdeg = jointDeg)
